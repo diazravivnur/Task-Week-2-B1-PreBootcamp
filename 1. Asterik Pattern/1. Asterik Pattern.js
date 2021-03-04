e7a7126388d07738c831dc2dcc84cc2d120efcb5
Diaz Raviv Nur <diazravivn@gmail.com>
2021-03-04 20:52:15 +0700

    Updated 1. Asterik Pattern

---
+++ 1. Asterik Pattern/1. Asterik Pattern.js
@@ -0,0 +1,19 @@
+// Buatlah sebuah function untuk mencetak pattern berikut beserta flow chartnya:
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+function asterikPattern () {
+  for(var i = 1; i <= 5; i++) {
+    for(var j = 1; j<= i; j++) {
+      document.write("*");  
+      }
+    document.write("<br/>");
+  }
+} 
+
+asterikPattern()
